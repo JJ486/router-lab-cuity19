e6c0d7e1ebd0dc66d162a01bbd49e6df48eab6ce
BladLust <33929696+BladLust@users.noreply.github.com>
2021-11-12 08:40:11 +0800

    HW1 Fin

--- Homework/eui64/eui64.cpp
+++ Homework/eui64/eui64.cpp
@@ -5,6 +5,17 @@
 in6_addr eui64(const ether_addr mac) {
   in6_addr res = {0};
   // TODO
-  res.s6_addr[0] = 0xfe;
+  res.__u6_addr.__u6_addr8[0]=0xFE;
+  res.__u6_addr.__u6_addr8[1]=0x80;
+  res.__u6_addr.__u6_addr8[11]=0xFF;
+  res.__u6_addr.__u6_addr8[12]=0xFE;
+  for (int i = 1; i < 4; i++)
+  {
+    res.__u6_addr.__u6_addr16[i]=0;// 3~8B=0
+    res.__u6_addr.__u6_addr8[7+i]=mac.octet[i-1];//9~11B=MAC1~3B
+    res.__u6_addr.__u6_addr8[12+i]=mac.octet[i+2];//14~16B=MAC4~6B
+  }
+  __uint8_t correctBit=0xFD|~(res.__u6_addr.__u6_addr8[8]&2);
+  res.__u6_addr.__u6_addr8[8]=(res.__u6_addr.__u6_addr8[8]|0x02)&correctBit;//Set it to correct vlu by and
   return res;
 }
