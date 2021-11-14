9099f6519a8d9888eccde0078b68849b6e9bd05b
BladLust <33929696+BladLust@users.noreply.github.com>
2021-11-14 21:25:54 +0800

    Finished HW3

--- Homework/lookup/lookup.cpp
+++ Homework/lookup/lookup.cpp
@@ -4,21 +4,133 @@
 #include <stdlib.h>
 #include <vector>
 
-void update(bool insert, const RoutingTableEntry entry) {
+std::vector<RoutingTableEntry> table;
+static int maxMatchLen(const in6_addr &lhs, const in6_addr &rhs, int maxLen = 128)
+{
+  int maxMatchLength = 0;
+
+  for (int i = 0; i < maxLen / 8 + 1; i++)
+  {
+    if (lhs.s6_addr[i] == rhs.s6_addr[i])
+    {
+      maxMatchLength += 8;
+      continue;
+    }
+    for (int j = 0; j < 8; j++, maxMatchLength++)
+    {
+      if ((lhs.s6_addr[i] & (0x80 >> j)) != (rhs.s6_addr[i] & (0x80 >> j)))
+      {
+        return maxMatchLength > maxLen ? maxLen : maxMatchLength;
+      }
+    }
+  }
+  return maxLen;
+}
+static void printAddr(const in6_addr& src){
+  char nexthop_buffer[128];
+  inet_ntop(AF_INET6, &src, nexthop_buffer,
+                         sizeof(nexthop_buffer));
+        printf("%s\n", nexthop_buffer);
+}
+static std::vector<RoutingTableEntry>::iterator maxMatch(const in6_addr &target)
+{
+  std::vector<RoutingTableEntry>::iterator it = table.begin();
+  int maxMatchLength = -1;
+  std::vector<RoutingTableEntry>::iterator maxMatchIt=table.end();
+  for (; it != table.end(); it++)
+  {
+    if (maxMatchLength >= (int)it->len)
+      continue;
+    if (maxMatchLength == 128)
+      return maxMatchIt;
+    int tempMaxLen=maxMatchLen(target, it->addr);
+    tempMaxLen=tempMaxLen>it->len?it->len:tempMaxLen;
+    if(tempMaxLen<it->len)
+    continue;
+    if (tempMaxLen > maxMatchLength)
+    {
+      // printf("tempMax=%d\n",tempMaxLen);
+      // printAddr(it->addr);
+      maxMatchIt = it;
+      maxMatchLength=tempMaxLen>it->len?it->len:tempMaxLen;
+      if (maxMatchLength == 128)
+        return maxMatchIt;
+    }
+  }
+  return maxMatchIt;
+}
+void update(bool insert, const RoutingTableEntry entry)
+{
+  for(auto it = table.begin();it!=table.end();it++){
+    if(it->len==entry.len){
+      int i=0;
+      for (; i < 16; i++)
+      {
+        if(it->addr.s6_addr[i]!=entry.addr.s6_addr[i])
+          break;
+      }
+      if(i==16){
+        table.erase(it);
+        break;
+      }
+    }
+  }
+  if (insert)
+    table.push_back(entry);
   // TODO
 }
 
-bool prefix_query(const in6_addr addr, in6_addr *nexthop, uint32_t *if_index) {
+bool prefix_query(const in6_addr addr, in6_addr *nexthop, uint32_t *if_index)
+{
   // TODO
+  auto it = maxMatch(addr);
+  if(it==table.end())
   return false;
+  for (int i = 0; i < 16; i++)
+  {
+    nexthop->s6_addr[i]=it->nexthop.s6_addr[i];
+  }
+  *if_index=it->if_index;
+  return true;
 }
 
-int mask_to_len(const in6_addr mask) {
+int mask_to_len(const in6_addr mask)
+{
   // TODO
-  return -1;
+  short prefLen = 128;
+  for (int i = 3; i >= 0; i--)
+  {
+    for (int j = 3; j >= 0; j--)
+    {
+      for (int k = 0; k < 8; k++)
+      {
+        if (mask.s6_addr[i * 4 + j] & (1 << k))
+          goto outside;
+        prefLen--;
+      }
+    }
+  }
+outside:
+  return prefLen;
 }
 
-in6_addr len_to_mask(int len) {
-  // TODO
-  return {};
+in6_addr len_to_mask(int len)
+{
+  if (len > 128 || len < 0)
+    return {};
+  in6_addr returnedMask;
+  for (int i = 0; i < 16; i++)
+  {
+    returnedMask.s6_addr[i] = 0;
+  }
+
+  for (int i = 0; i < len / 8; i++)
+  {
+    returnedMask.s6_addr[i] = 0xFF;
+  }
+  for (int i = 0; i < len % 8; i++)
+  {
+    returnedMask.s6_addr[len / 8] |= 0x80 >> i;
+  }
+  return returnedMask;
 }
